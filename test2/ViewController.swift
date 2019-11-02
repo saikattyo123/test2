c7daaf4bdefb08a5ba5138b714074ea3cb7f79c2
斎藤克哉 <katsuya12321@gmail.com>
2019-11-02 18:33:50 +0900

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by 斎藤克哉 on 2019/11/02.
+//  Copyright © 2019 PeerRadio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
